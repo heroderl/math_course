d997e57061598b1b862d923eb966d22f2ac0ee61
Dengjunle <46371556+Dengjunle@users.noreply.github.com>
2019-01-06 19:58:47 +0800

    djl:update

--- admin/config/config.js
+++ admin/config/config.js
@@ -4,14 +4,10 @@
 
 var urlConfig = (function() {
 	return {
-<<<<<<< HEAD
 		'host': '219z27o988.iask.in',
 		'post': '54483',
-=======
-//		'host': '10.10.113.103',
-		'host': '10.0.1.53',
-		'post': '8080',
->>>>>>> ec4271c6c87cbf0f6d7def7b1adab0a9e120ef9f
+		//'host': '10.0.1.53',
+		//'post': '8080',
 		'com': '/math_course/common/index.php/'
 	}
 })();
