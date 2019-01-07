1af9199d886c57fa14a334ca5dbf7e97f53cfab6
Zhengliwan <870249040@qq.com>
2019-01-07 19:04:04 +0800

    zlw:update

--- admin/AddQuestion.php
+++ admin/AddQuestion.php
@@ -31,7 +31,6 @@ class AddQuestion{
 			$q_content=$_POST["q_content"];//试题内容
 			$board=trim($_POST["board"]);//json格式字符
 			$q_board=$board;//试题画板
-			
 			//if(isset($_COOKIE['username']))//
 			if($_COOKIE['PHPSESSID']==$phpsessid)
 			{	
@@ -43,8 +42,9 @@ class AddQuestion{
 					}else{
 						$sql="select * from question where q_No={$q_No}";
 						$num=$this->conn->selectBySql($sql);//检查试题编号是否已存在数据库中，存在count($num['data'])大于0，不能存在count($num['data'])等于0
+						
 						if(count($num['data'])==0)
-						{           
+						{   
 							$sql="INSERT INTO `math`.`question` (`q_id`, `q_No`, `q_content`, `q_state`, `q_time`, `q_board`, `q_other`) VALUES (NULL, '$q_No', '$q_content', '0', current_date(), '$q_board', '');";
 							$result=$this->conn->insertBySql($sql);//添加试题到question表中		
 							if($result['code']==200)
@@ -54,11 +54,16 @@ class AddQuestion{
 							}else
 							{	header("Access-Control-Allow-Origin: *"); // 允许任意域名发起的跨域请求  
 								$this->conn->json($result, 200, '添加错误');
+								
 							}
 						}else
 						{
-							echo "<script>alert('添加失败，此试题编号已存在！')</script>";
-							echo "<script>document.location='../admin/html/addTestQuest.html'</script>";
+							$result = array(
+						            'code'      => 0,
+						            'message'   => 0,
+						            'data'      => "添加失败，此试题编号已存在！"
+						        );
+							$this->conn->json($result);//返回json格式数据	
 						}
 					}	
 				}
