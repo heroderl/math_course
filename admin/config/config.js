9bb20d4b5b47025d218354be82febcbae16bb609
Zhengliwan <870249040@qq.com>
2019-01-07 16:58:24 +0800

    Update config.js

--- admin/config/config.js
+++ admin/config/config.js
@@ -4,10 +4,8 @@
 
 var urlConfig = (function() {
 	return {
-		'host': '219z27o988.iask.in',
-		'post': '54483',
-		//'host': '10.0.1.53',
-		//'post': '8080',
+		'host': '10.10.113.103',
+		'post': '8080',
 		'com': '/math_course/common/index.php/'
 	}
 })();
